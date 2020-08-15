7e977d2e2fc53af72754330272f90f3b99cd7a2e
Yiyang Zhan <pon.zhan@gmail.com>
2020-08-15 12:35:50 +0900

    Spring Initializr

---
+++ camel-jms/src/main/java/dev/zhanpon/cameljms/CamelJmsApplication.java
@@ -0,0 +1,13 @@
+package dev.zhanpon.cameljms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelJmsApplication.class, args);
+	}
+
+}
---
+++ camel-jms/src/test/java/dev/zhanpon/cameljms/CamelJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zhanpon.cameljms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
